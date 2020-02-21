c71fcf8d17bd23e374c087767badc8b169ad9c37
vp090391 <vp090391@gmail.com>
2020-02-21 11:50:49 +0300

    fix: change API key for forecast and make screenshot

--- src/js/loadImage.js
+++ src/js/loadImage.js
@@ -2,7 +2,7 @@ export default function loadImage (searchField) {
     const citySave = document.getElementById('main-search-searchField').value;
 
     if (searchField) {
-        const url = `https://api.unsplash.com/photos/random?query=${searchField}&client_id=a85731e056bddacdfa28e14195f55378a793e7053601f0b195faeab10b0178c8`;
+        const url = `https://api.unsplash.com/photos/random?query=${searchField}&client_id=E93ah58UBXrmi4s9gDj6fYnJ_U_jdUvF_jpZouatjE0`;
         fetch(url)
             .then(res => res.json())
             .then(data => data.urls.regular )
@@ -15,7 +15,7 @@ export default function loadImage (searchField) {
             .then(data => {
                 // Take city for background image
                 const city = data.response.GeoObjectCollection.featureMember[0].GeoObject.name;
-                const url = `https://api.unsplash.com/photos/random?query=${city}&client_id=a85731e056bddacdfa28e14195f55378a793e7053601f0b195faeab10b0178c8`;
+                const url = `https://api.unsplash.com/photos/random?query=${city}&client_id=E93ah58UBXrmi4s9gDj6fYnJ_U_jdUvF_jpZouatjE0`;
                 fetch(url)
                     .then(res => res.json())
                     .then(info => info.urls.regular )
