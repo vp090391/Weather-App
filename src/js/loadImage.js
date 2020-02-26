cf6baae3025735363ffb26565413f9af41cc879c
vp090391 <vp090391@gmail.com>
2020-02-26 11:46:03 +0300

    fix: changeImage button bug

--- src/js/loadImage.js
+++ src/js/loadImage.js
@@ -1,6 +1,4 @@
 export default function loadImage (searchField) {
-    const citySave = document.getElementById('main-search-searchField').value;
-
     if (searchField) {
         const url = `https://api.unsplash.com/photos/random?query=${searchField}&client_id=E93ah58UBXrmi4s9gDj6fYnJ_U_jdUvF_jpZouatjE0`;
         fetch(url)
@@ -10,6 +8,8 @@ export default function loadImage (searchField) {
                 document.querySelector('.body').style.backgroundImage = `url('${link}')`;
             });
     } else {
+        let citySave = document.getElementById("main-geoPosition").innerHTML;
+
         fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=a77fffe6-e143-4f4c-b9c0-e5a4f98c6a07\n&format=json&geocode=${citySave}&lang=en_US`)
             .then(res => res.json())
             .then(data => {
